de84b53ae8716404d8de7f739945a3612adabcca
adindagl <adindalestari1507@gmail.com>
2023-09-26 14:59:06 +0700

    penambahan isi application/controllers/Contoh1.php ;

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan Diri</h1>";
+        echo "Nama : Adinda Gita Lestari <br>";
+        echo "Nim  : 12221333 <br>";
+        echo "Kelas: 12.3A.05 <br>";
+        echo "TTL  : Bekasi, 14 September 2003 <br>";
+        echo "Hobi : Membaca dan Mendengar Musik"; 
+    }
+}
